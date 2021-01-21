e40e1fda15425421a07a60caf8790492aeb22d5e
Leonardo <oliveirabalsa2@gmail.com>
2021-01-21 01:20:27 -0300

    refator: change appointment Service

---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,36 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '../infra/typeorm/entities/Appointment.model';
+import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
+
+interface Request {
+  date: Date;
+  provider_id: string;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = await appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
