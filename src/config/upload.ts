ae0e217163fa2444d44753b420e277899301e879
Leonardo Balsalobre <oliveirabalsa2@gmail.com>
2021-06-25 00:45:07 -0300

    feat: implement storage provider members

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,16 +1,18 @@
-import multer from 'multer';
-import crypto from 'crypto';
 import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tmpFolder,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
+      const filehash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${filehash}-${file.originalname}`;
 
       return callback(null, fileName);
     },
